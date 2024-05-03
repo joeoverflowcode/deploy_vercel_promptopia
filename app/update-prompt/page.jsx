ae4421bdafcda64e0fbb85420411c2eeb345d0c9
Joe Aguado <aguado.joe@gmail.com>
2024-05-03 09:55:35 -0500

    added: suspense boundary update-prompt

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -2,9 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-
 import { Suspense } from "react";
-
 import Form from "@components/Form";
 
 const UpdatePrompt = () => {
