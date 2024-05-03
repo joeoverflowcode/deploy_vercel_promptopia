23847a5aab221dac5960ed273112c7786ff06543
Joe Aguado <aguado.joe@gmail.com>
2024-05-03 15:19:48 -0500

    added: username params

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import { Schema, model, models } from "mongoose";
+import { unique } from "next/dist/build/utils";
 
 const UserSchema = new Schema({
     email: {
@@ -8,6 +9,7 @@ const UserSchema = new Schema({
     },
     username: {
         type: String,
+        unique: [false, 'Pick a Name'],
         required: [true, 'Username is required!'],
         match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
       },
