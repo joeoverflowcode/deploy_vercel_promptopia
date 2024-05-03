c8b515885d9785d206014eca512414711ff25d83
Joe Aguado <aguado.joe@gmail.com>
2024-05-03 15:23:03 -0500

    added: username params

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const UserSchema = new Schema({
     },
     username: {
         type: String,
-        unique: [false, 'Pick a Name'],
+        // unique: [false, 'Pick a Name'],
         required: [true, 'Username is required!'],
         match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
       },
